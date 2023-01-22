a46e119b613101475a4d4d2733040c9f5f7da4fe
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2023-01-22 14:32:33 -0300

    ECS:   - Fix docker   - Fix pipeline

--- main.go
+++ main.go
@@ -1,18 +1,17 @@
 package main
 
 import (
-	"log"
-
-	"github.com/joho/godotenv"
+	// "log"
+	// "github.com/joho/godotenv"
 	"github.com/rafaelcarvalhocaetano/ci_cd_golang/database"
 	"github.com/rafaelcarvalhocaetano/ci_cd_golang/routes"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Some error occured. Err: %s", err)
-	}
+	// err := godotenv.Load(".env")
+	// if err != nil {
+	// 	log.Fatalf("Some error occured. Err: %s", err)
+	// }
 	database.ConectaComBancoDeDados()
 	routes.HandleRequest()
 }
